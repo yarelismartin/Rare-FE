3e6f65cd25fa79f8db34442a4c6fb9697caa231e
Alex Berka <alexanderberka@gmail.com>
2024-09-18 21:15:09 -0500

    Create tag in Tag Manager

--- pages/tags.js
+++ pages/tags.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TagTable from '../components/tables/TagTable';
+import { getAllTags } from '../api/tagData';
+import TagForm from '../components/forms/TagForm';
 
 export default function TagManager() {
+  const [tags, setTags] = useState([]);
+
+  const updateTags = () => {
+    getAllTags().then(setTags);
+  };
+
+  useEffect(() => {
+    updateTags();
+  }, []);
+
   return (
     <>
       <h1 className="display-1 mb-4">Tag Manager</h1>
-      <TagTable />
+      <TagTable tags={tags || []} />
+      <TagForm onUpdate={updateTags} />
     </>
   );
 }
